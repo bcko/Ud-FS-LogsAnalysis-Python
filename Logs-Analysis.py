d5665c1cc63c34bc469778123006522ae926b2be
Byungcheon Ko <bcko@users.noreply.github.com>
2017-06-06 05:41:27 -0400

    declare main()

--- Logs-Analysis.py
+++ Logs-Analysis.py
@@ -2,3 +2,11 @@
 
 # psycopg is the PostgreSQL adapter for the Python programming language. 
 import psycopg2 
+
+
+def main():
+  
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
