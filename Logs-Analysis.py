1c3bf2f0ad0fe43afde201903598e962014e47a4
Byungcheon Ko <bcko@users.noreply.github.com>
2017-06-06 06:12:43 -0400

    three questions

--- Logs-Analysis.py
+++ Logs-Analysis.py
@@ -8,12 +8,35 @@ import psycopg2
 
 def main():
   
+  question1 = "1. What are the most popular three articles of all time?\n\
+  Which articles have been accessed the most?\n\
+  Present this information as a sorted list with the most popular article at the top\n"
+  
+  question2 = "2. Who are the most popular article authors of all time?\n\
+  That is, when you sum up all of the articles each author has written, which authors get the most page views?\n\
+  Present this as a sorted list with the most popular author at the top.\n"
+  
+  question3 = "3. On which days did more than 1% of requests lead to errors?\n\
+  The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser.\n"
+  
   # Connect to an existing database
   conn = psycopg2.connect("dbname=news")
   
   # Open a cursor to perform database operations
   cur = conn.cursor()
 
+  
+  
+  print(question1)
+  
+  
+  print(question2)
+  
+  
+  print(question3) 
+
+  
+  
   
   # Close communication with the database
   cur.close()
