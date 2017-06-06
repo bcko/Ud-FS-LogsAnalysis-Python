5453d33f3d914196a15c01d70663573146109ffe
Byungcheon Ko <bcko@users.noreply.github.com>
2017-06-06 05:54:59 -0400

    Update Logs-Analysis.py

--- Logs-Analysis.py
+++ Logs-Analysis.py
@@ -11,10 +11,12 @@ def main():
   # Connect to an existing database
   conn = psycopg2.connect("dbname=news")
   
-  
+  # Open a cursor to perform database operations
   cur = conn.cursor()
 
   
+  # Close communication with the database
+  cur.close()
   conn.close()
   
 if __name__ == "__main__":
