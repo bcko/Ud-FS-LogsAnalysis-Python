0bdae3ccd9695b876371fbdb8d7105b7a582282f
Byungcheon Ko <bcko@users.noreply.github.com>
2017-06-08 00:01:01 -0400

    q1 sql

--- Logs-Analysis.py
+++ Logs-Analysis.py
@@ -7,16 +7,42 @@ Some database code and comments are adapted from the psycopg documentation(http:
 import psycopg2 
 
 def main():  
-  question1 = """ 1. What are the most popular three articles of all time?
-  Which articles have been accessed the most?
-  Present this information as a sorted list with the most popular article at the top"""
-
-  question2 = """2. Who are the most popular article authors of all time?
-  That is, when you sum up all of the articles each author has written, which authors get the most page views?
-  Present this as a sorted list with the most popular author at the top."""
   
-  question3 = """3. On which days did more than 1% of requests lead to errors?
-  The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser."""
+  question1 = """ 
+  1. What are the most popular three articles of all time?
+  Which articles have been accessed the most?
+  Present this information as a sorted list with the most popular article at the top
+  """
+
+  q1_SQL = """ 
+  SELECT articles.title, COUNT(log.path) as num_access 
+  FROM articles, log
+  WHERE log.path = '/article/' || articles.slug
+  GROUP BY articles.title
+  ORDER BY num_access DESC
+  LIMIT 3;
+  """
+  
+  question2 = """
+  2. Who are the most popular article authors of all time?
+  That is, when you sum up all of the articles each author has written, which authors get the most page views?
+  Present this as a sorted list with the most popular author at the top.
+  """
+  
+  q2_SQL = """
+  SELECT authors.name
+  FROM
+  WHERE
+  """
+  
+  question3 = """
+  3. On which days did more than 1% of requests lead to errors?
+  The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser.
+  """
+  
+  q3_SQL = """
+  
+  """
 
   
   
