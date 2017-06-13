1a22aee9389f9255d5c22e0bd234b6fc5a0df92c
Byungcheon Ko <bcko@users.noreply.github.com>
2017-06-13 18:03:12 -0400

    Create Logs-Analysis.py

--- Logs-Analysis.py
+++ Logs-Analysis.py
@@ -7,14 +7,27 @@ Some database code and comments are adapted from the psycopg documentation(http:
 import psycopg2 
 
 def main():  
+  # Connect to an existing database
+  conn = psycopg2.connect("dbname=news")
   
+  # Open a cursor to perform database operations
+  cur = conn.cursor()
+  
+  # Question 1
   sql_popular_articles = """ 
-    SELECT * 
+    SELECT article_view.title, article_view.view
     FROM article_view 
     ORDER BY article_view.view DESC
     LIMIT 3;
   """
+  cur.execute(sql_popular_articles)
+  print("Most popular articles:")
+  for (title, view) in cur.fetchall():
+    print("    {} - {} views".format(title, view))
+  print("-" * 70)
 
+  
+  # Question 2
   sql_popular_authors = """
     SELECT article_view.name, SUM(article_view.view) AS author_view
     FROM article_view
@@ -22,27 +35,25 @@ def main():
     ORDER BY author_view DESC
     LIMIT 1;
   """
+  cur.execute(sql_popular_authors)
+  print("Most popular authors:")
+  for (name, view) in cur.fetchall():
+    print("    {} - {} views".format(name, view))
+  print("-" * 70)
   
+  # Question 3
   sql_more_than_one_percent_errors = """
     SELECT *
     FROM error_rate
     WHERE error_rate.percentage > 1
     ORDER BY error_rate.percentage DESC;
   """
-  
-  # Connect to an existing database
-  conn = psycopg2.connect("dbname=news")
-  
-  # Open a cursor to perform database operations
-  cur = conn.cursor()
-  
-  
+  cur.execute(sql_more_than_one_percent_errors)
+  print("Days with more than 1% errors:")
+  for (date, percentage) in cur.fetchall():
+    print("    {} - {}% errors".format(date, percentage))
+  print("-" * 70)
 
-  
- 
-
-  
-  
   
   # Close communication with the database
   cur.close()
